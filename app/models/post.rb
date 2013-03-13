b905028ca3926b3a4f28128627f7f76a4d11f2b5
Jeremy Muljana <jermul714@gmail.com>
2013-03-12 17:29:11 -0700

    bootstrappin

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,22 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title, :tags_attributes
 
-  validates :name,  :presence => true
-  validates :title, :presence => true,
-  					:length => { :minimum => 5 }
+  validates :name,  presence: true
+  validates :title, presence: true,
+  					length: { :minimum => 5 }
 
-  has_many :comments, :dependent => :destroy
+  has_many :comments, dependent: :destroy
   has_many :tags
 
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+
+  def self.search(search)
+
+  	if search
+  	  where 'name ILIKE ? OR title ILIKE? OR content ILIKE?', "%#{search}%", "%#{search}%", "%#{search}%"
+    else
+      scoped
+    end
+  end
 end
